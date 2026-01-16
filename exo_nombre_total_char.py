8262aeb5ec0a6cd24ff2ab3cc38ea030b9686381
10382249 <github.com.deflator660@passmail.net>
2026-01-16 19:31:17 +0900

    16/01 v2

---
+++ exo_nombre_total_char.py
@@ -0,0 +1,14 @@
+noms= ["Jean","Sophie","Martin","Christophe","Zoé","Martin"]
+
+'''nb_char =0
+for nom in noms:
+    nb_char+=len(nom)
+print(nb_char)'''
+
+'''list_length=[len(nom) for nom in noms]
+nb_char=sum(list_length)
+print(nb_char)'''
+
+list_length=[len(nom) for nom in noms]
+nb_char=
+print(nb_char)
